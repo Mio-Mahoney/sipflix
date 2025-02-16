29bb09c9311f1eb14c5c17e4953ba1bd3a6def78
Mio Mahoney <mmahone5@oswego.edu>
2025-02-15 23:37:28 -0500

    Removed button

--- frontend/src/lib/ModeSelect.svelte
+++ frontend/src/lib/ModeSelect.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 </script>
 
-<div class="bg-skyblue-500 w-[400px] max-w[25%] h-450 text-skybue-500">
+<div class="bg-skyblue-500 w-[400px] max-w[25%] max-h[500px] text-skybue-500">
     <button class="bg-black">Drunk O' Meter</button>
-    <button>Pick a word</button>
-    <button>Random Word</button>
 </div>
