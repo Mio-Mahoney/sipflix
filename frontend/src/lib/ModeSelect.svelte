961532f7d72fe482d186e89c0e794196fe453737
Mio Mahoney <mmahone5@oswego.edu>
2025-02-15 23:22:26 -0500

    Added ModeSelect

---
+++ frontend/src/lib/ModeSelect.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="bg-skyblue-500 w-[400px] max-w[25%] h-450 text-skybue-500">
+    <button class="bg-black">Drunk O' Meter</button>
+    <button>Pick a word</button>
+    <button>Random Word</button>
+</div>
