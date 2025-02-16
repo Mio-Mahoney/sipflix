0891a64c03d43656d95e0f08da7450d56921bcc1
clxxiii <clxxiii@users.noreply.github.com>
2025-02-15 23:22:12 -0500

    color scheme update

---
+++ frontend/src/lib/Search.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Globe } from "lucide-svelte";
+	import type { FormEventHandler } from "svelte/elements";
+
+	const oninput: FormEventHandler<HTMLInputElement> = (ev) => {
+		console.log(ev);
+	};
+</script>
+
+<div class="w-[800px] max-w-[95%] relative">
+	<div class="absolute top-3 left-3 text-neon-carrot-600">
+		<Globe />
+	</div>
+	<input
+		{oninput}
+		class="h-12 rounded-full w-full bg-neon-carrot-200 text-neon-carrot-700 pl-12 border-4 border-neon-carrot-500 outline-none"
+		placeholder="Paste in a youtube link..."
+		type="text"
+	/>
+</div>
